1806b7734724a056b41a3613816a146f14a65160
quanganhtapcode <quanganhtapcode@gmail.com>
2026-01-08 21:47:38 +0700

    Remove unused cors import

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@
  */
 
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const multer = require('multer');
 const fetch = require('node-fetch');
 const FormData = require('form-data');
